2b6fe07994b9b353e7ae5208881231de94b885e2
sunnygodiwal1991 <60594463+sunnygodiwal1991@users.noreply.github.com>
2024-11-11 01:51:19 +0530

    Update buildStage.groovy

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -2,7 +2,9 @@
 
 def call() {
     stage('Build') {
+        def mavenVersion = '3.8.1'  // Default version for most jobs
         echo "Building the project..."
+        echo "Setting up Maven version: ${mavenVersion}"
         // Add your build logic here (e.g., `mvn clean install`, `npm build`, etc.)
     }
     stage('test') {
